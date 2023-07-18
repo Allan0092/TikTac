4cfb7bc1c0f66f9cec103ebe4818de083ffe9a9a
allan <allan.gtm92@gmail.com>
2023-07-18 20:08:00 +0545

    added quit button

--- tiktac2.py
+++ tiktac2.py
@@ -2,14 +2,18 @@ import pygame
 
 
 class Tiktak:
-    def __init__(self):  # sets logo, displays plain white window
-        print('initializing')
-        logo = pygame.image.load('/home/gtm/Documents/code/tik tak project/mats/logo.png')
+    def __init__(self):  
+        """
+            Sets logo, displays plain white window
+        """
+
+        #print('initializing')
+        #logo = pygame.image.load('/home/gtm/Documents/code/tik tak project/mats/logo.png')
         self.running = True
         pygame.init()
         self.black = (255, 255, 255, 255)
         self.white = (0, 0, 0, 0)
-        pygame.display.set_icon(logo)
+        #pygame.display.set_icon(logo)
         pygame.display.set_caption('TikTak2.0')
         self.xaxis = 800
         self.yaxis = 800
@@ -23,37 +27,45 @@ class Tiktak:
         self.board()
         self.playing()
 
-    def gameover(self):  # menu shown after game is over
-        print('func game over')
+    def gameover(self):  
+        """
+            Menu shown after game is over
+        """
+
+        #print('func game over')
         pygame.time.delay(800)
         textsurface = pygame.font.SysFont('Ariel', 40, True).render('GAME OVER', False, self.black)
         self.screen.fill(self.white)
         self.screen.blit(textsurface, (self.line_x, self.line_y))
         if self.isSolved()[1] == 1:
-            print('x won')
+            #print('x won')
             winner_surface = pygame.font.SysFont('Ariel', 40, True).render('X HAS WON', False, self.black)
             self.screen.blit(winner_surface, (self.line_x * 1.01, self.line_y * 1.2))
             pygame.display.flip()
             self.display_quit()
         elif self.isSolved()[1] == 2:
-            print('O won')
+            #print('O won')
             winner_surface = pygame.font.SysFont('Ariel', 40, True).render('O HAS WON', False, self.black)
             self.screen.blit(winner_surface, (self.line_x * 1.01, self.line_y * 1.2))
             pygame.display.flip()
             self.display_quit()
         elif self.isSolved()[1] == 0:
-            print('draw')
+            #print('draw')
             winner_surface = pygame.font.SysFont('Ariel', 40, True).render('DRAW', False, self.black)
             self.screen.blit(winner_surface, (self.line_x * 1.25, self.line_y * 1.2))
             pygame.display.flip()
             self.display_quit()
 
-    def display_quit(self):  # quits display
+    def display_quit(self): 
+        """
+            Quits display
+        """
+
         if self.play_again():
             s = Game()
         try:
             loop = True
-            print('display_quit func')
+            #print('display_quit func')
             while loop:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
@@ -63,8 +75,12 @@ class Tiktak:
             pass
         self.running = False
 
-    def turns(self, drawing):  # taking turns
-        print('func turns')
+    def turns(self, drawing):  
+        """
+            Manages turns for players.
+        """
+
+        #print('func turns')
         if self.turn:
             for i in range(3):
                 for ii in range(3):
@@ -82,13 +98,21 @@ class Tiktak:
             self.turn = True
             self.crosss(drawing)
 
-    def circle(self, dra):  # draws circle
+    def circle(self, dra):  
+        """
+            Draws circle in the board.
+        """
+
         radius = 2 * self.cross
         center = [(dra[0][0] + dra[1][0]) / 2, (dra[0][1] + dra[1][1]) / 2]
         pygame.draw.circle(self.screen, self.black, center, radius, 10)
         pygame.display.update()
 
-    def crosss(self, dr):  # draws cross
+    def crosss(self, dr):  
+        """
+            Draws cross on the board.
+        """
+
         c1 = [dr[0][0] + self.cross, dr[0][1] + self.cross]
         c2 = [dr[1][0] - self.cross, dr[1][1] - self.cross]
         c3 = [dr[2][0] - self.cross, dr[2][1] + self.cross]
@@ -97,8 +121,12 @@ class Tiktak:
         pygame.draw.line(self.screen, self.black, c3, c4, 10)
         pygame.display.update()
 
-    def board(self):  # draws 4 lines for boardd and separates into 9 boxes
-        print('func board')
+    def board(self):  
+        """
+            Draws 4 lines for boardd and separates into 9 boxes.
+        """
+
+        #print('func board')
         self.line_x = self.xaxis / 3
         self.line_y = self.yaxis / 3
         self.line1, self.line2, self.line3, self.line4 = (self.line_x, 0), (self.line_x * 2, 0), (0, self.line_y), (
@@ -119,12 +147,16 @@ class Tiktak:
                      (2 * self.line_x, 2 * self.line_y)]
         self.box7 = [(0, 2 * self.line_y), (self.line_x, self.yaxis), (self.line_x, 2 * self.line_y), (0, self.yaxis)]
         self.box8 = [(self.line_x, 2 * self.line_y), (2 * self.line_x, self.yaxis), (2 * self.line_x, 2 * self.line_y),
-                     (self.line_x, self.yaxis)]
+                    (self.line_x, self.yaxis)]
         self.box9 = [(2 * self.line_x, 2 * self.line_y), (self.xaxis, self.yaxis), (self.xaxis, 2 * self.line_y),
                      (2 * self.line_x, self.yaxis)]
         pygame.display.flip()
 
-    def isSolved(self):  # rules of the game; return 1:X , 2:O , 0:draw , -1:not_over
+    def isSolved(self):  
+        """
+            Rules of the game; return 1:X , 2:O , 0:draw , -1:not_over
+        """
+
         boardd = self.boardd
         for i in range(0, 3):
             if boardd[i][0] == boardd[i][1] == boardd[i][2] != 0:
@@ -141,8 +173,12 @@ class Tiktak:
         else:
             return False, -1
 
-    def playing(self):  # looping the actual game
-        print('playing')
+    def playing(self):  
+        """
+            Looping the actual game.
+        """
+
+        #print('playing')
         while self.running:
             for event in pygame.event.get():
                 pygame.display.update()
@@ -208,20 +244,34 @@ class Tiktak:
                             self.boardd[2][2] = 9
                             self.turns(self.box9)
             if self.isSolved()[0] == True:
-                print('The game seems to be over')
+                #print('The game seems to be over')
                 # self.running=False
                 self.gameover()
 
-    def play_again(self):  # Asks if user wants to play again
-        print('func play_again')
+    def play_again(self):  
+        """
+            Asks if user wants to play again.
+        """
+        
+        # Draws the rectriangle around play again.
         left = self.line_x
         top = self.line_y * 1.5
         width = self.line_x * 0.93
         height = self.line_y * 0.25
         rect = pygame.Rect(left, top, width, height)
         pygame.draw.rect(self.screen, self.black, rect)
-        play_again_surface = pygame.font.SysFont('Ariel', 30, True).render('play again', False, self.white)
+
+        # Draws rectriangle around Quit game.
+        top1 = self.line_y * 2
+        rect = pygame.Rect(left, top1, width, height)
+        pygame.draw.rect(self.screen, self.black, rect)
+
+
+
+        play_again_surface = pygame.font.SysFont('Ariel', 30, True).render('Play Again', False, self.white)
+        exit_game_surface = pygame.font.SysFont('Ariel', 30, True).render("Exit", False, self.white)
         self.screen.blit(play_again_surface, (self.line_x * 1.175, self.line_y * 1.6))
+        self.screen.blit(exit_game_surface, (self.line_x*1.175, self.line_y*2.1))
         pygame.display.flip()
         click = True
         while click:
@@ -234,15 +284,22 @@ class Tiktak:
                     pos = pygame.mouse.get_pos()
                     if pos[0] > left and pos[0] < left + width and pos[1] > top and pos[1] < top + height:
                         return True
+                    elif pos[0] > left and pos[0] < left + width and pos[1] > top1 and pos[1] < top1 + height:
+                        pygame.quit()
+                        return False
 
-    def winner_lines(self,line1,line2):  # Draw the lines after someone has won
-        print(f"draw lines func")
+    def winner_lines(self,line1,line2):  
+        """
+        Draw the lines after someone has won.
+        """
+        
+        #print(f"draw lines func")
         pygame.draw.line(self.screen,self.black,line1,line2,2)
 
 class Game:
 
     def __init__(self):
-        print('class Game')
+        #print('class Game')
         g = Tiktak()
 
 if __name__ == "__main__":
